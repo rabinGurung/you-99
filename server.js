b62e062d2bd1bba1a74e036e91ad966158e23f4d
leonul <leongurung029@gmail.com>
2019-05-08 08:24:25 +0545

    all reacts

--- server.js
+++ server.js
@@ -13,10 +13,6 @@ app.use((req,res,next)=>{
 app.use(bodyParser.json())
 
 
-app.get('/',(req,res)=>{
-    res.end('<h1> Hello World!</h1>')
-})
-
 app.get('/login',(req,res)=>{
     Data = [
         {
